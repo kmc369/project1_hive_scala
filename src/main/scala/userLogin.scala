a531572881ba30a21cccecae992608e4879c15b2
Zen W <zenw@Zens-MacBook-Air.local>
2022-01-31 19:10:26 -0500

    EVERTHING DONE BUT BUCKETS

--- src/main/scala/userLogin.scala
+++ src/main/scala/userLogin.scala
@@ -167,7 +167,7 @@ do{
      println()
      println("8) EXIT")
     
-     var a = scan.nextInt()
+     var a = scan.nextInt() 
 
 
 
@@ -179,53 +179,73 @@ do{
          //output.limit(0).show() 
   
   
-   // output.createOrReplaceTempView("temp_data")
-        hiveCtx.sql("USE project1_hive;")
+  output.createOrReplaceTempView("temp_data")
+        hiveCtx.sql("USE project1_hive_scala;")
   
     
         
-        //hiveCtx.sql("CREATE TABLE IF NOT EXISTS Questions_table (datetime String, city STRING, state STRING, country string, shape STRING, duration_seconds String, duration_HoursMin String, comments String, date_posted STRING,latitude STRING, Lngitude STRING) row format delimited fields terminated by ',' stored as textfile; ") 
-        //hiveCtx.sql("INSERT INTO Questions_table SELECT * FROM temp_data")
+        hiveCtx.sql("CREATE TABLE IF NOT EXISTS ufo_data (datetime String, city STRING, state STRING, country string, shape STRING, duration_seconds String, duration_HoursMin String, comments String, date_posted STRING,latitude STRING, Longitude STRING) row format delimited fields terminated by ',' stored as textfile; ") 
+        hiveCtx.sql("INSERT INTO ufo_data SELECT * FROM temp_data")
           
-      //val questions = hiveCtx.sql("SELECT * FROM LIMIT 10;")
-      //questions.show()
-  
+      val questions = hiveCtx.sql("SELECT * FROM ufo_data LIMIT 10;")
+     // questions.show()
+
+      hiveCtx.sql("CREATE TABLE IF NOT EXISTS ufo_data_part(datetime String, city STRING, country string, shape STRING, duration_seconds String, duration_HoursMin String, comments String, date_posted STRING,latitude STRING, Longitude STRING) Partitioned by (state String) row format delimited fields terminated by ',' stored as textfile; ") 
+      hiveCtx.sql(" set hive.exec.dynamic.partition.mode=nonstrict;")
+      hiveCtx.sql("INSERT INTO ufo_data_part SELECT datetime String, city STRING, country string, shape STRING, duration_seconds String, duration_HoursMin String, comments String, date_posted STRING,latitude STRING, Longitude STRING , state String  FROM ufo_data;")
+
+      val part = hiveCtx.sql("select * from ufo_data_part limit 10")
+      //part.show()
+
+    
+      hiveCtx.sql("CREATE TABLE IF NOT EXISTS ufo_data_bucket(datetime String, city STRING, country string, shape STRING, duration_seconds String, duration_HoursMin String, comments String, date_posted STRING,latitude STRING, Longitude STRING) Partitioned by (state String) CLUSTERED BY (city) into 4 buckets row format delimited fields terminated by ',' stored as textfile; ") 
+      hiveCtx.sql("set hive.enforce.bucketing=true;")
+      hiveCtx.sql(" set hive.exec.dynamic.partition.mode=nonstrict;")
+      hiveCtx.sql("INSERT INTO ufo_data_bucket SELECT datetime String, city STRING, country string, shape STRING, duration_seconds String, duration_HoursMin String, comments String, date_posted STRING,latitude STRING, Longitude STRING , state String  FROM ufo_data;")
+      
+      val buck = hiveCtx.sql("select * from ufo_data_bucket limit 10")
+      //buck.show()
+   
+   
+   
+   
+   
     if(a == 1){
        
-      val country_max = hiveCtx.sql("SELECT count(country) as totalsighting, country from ufo_808_bucket group by country order by totalsighting desc limit 1;")
+      val country_max = hiveCtx.sql("SELECT count(country) as totalsighting, country from ufo_data_part group by country order by totalsighting desc limit 1;")
       country_max.show()
 
     }
      if(a == 2){
        
-     val max_encounter = hiveCtx.sql("Select MAX(duration_seconds) as encountertime, state from ufo_808_bucket WHERE country = 'us' group by state order by  encountertime DESC limit 5;")
+     val max_encounter = hiveCtx.sql("Select MAX(duration_seconds) as encountertime, state from ufo_data_part WHERE country = 'us' group by state order by  encountertime DESC limit 5;")
       max_encounter.show()
 
       //WHAT IS THE LONGEST ENCOURTERS SOMEONE HAS HAD WITH A UFO IN THE UNITED STATES //help with this query
     }
      if(a == 3){
        
-      val ufo_shape = hiveCtx.sql("SELECT Count(shape) as totalshape ,shape  FROM ufo_808_bucket group by shape order by totalshape desc limit 5;")
+      val ufo_shape = hiveCtx.sql("SELECT Count(shape) as totalshape ,shape  FROM ufo_data_part group by shape order by totalshape desc limit 5;")
       ufo_shape.show();
 
       //WHAT IS THE MOST COMMON SHAPE OF UFO'S THAT ARE IDENTIFIED?
     
     }
     if (a ==4){
-     val city_shape = hiveCtx.sql("SELECT AVG(duration_seconds) FROM ufo_808_bucket WHERE state = 'nj';")
+     val city_shape = hiveCtx.sql("SELECT AVG(duration_seconds) FROM ufo_data_part WHERE state = 'nj';")
     city_shape.show()
     //WHAT IS THE AVERAGE TIME SOMEONE HAS SEEN A UFO IN NEW JERSEY
     }
     
   if (a==5){
-    val total_time = hiveCtx.sql("SELECT Min(shape) as possibleshape, shape from ufo_808_bucket group by shape order by possibleshape ASC limit 2; ")
+    val total_time = hiveCtx.sql("SELECT Min(shape) as possibleshape, shape from ufo_data_part group by shape order by possibleshape ASC limit 2; ")
     total_time.show();
     //WHAT SHAPE IS MOSTLY LIKELY TO ME CONFUSED AS A UFO?
   
   }
 
   if(a==6){
-    val least_likely = hiveCtx.sql("SELECT count(state) as leastlikely,state from ufo_808_bucket where country = 'us'  group by state order by leastlikely ASC limit 5; ")
+    val least_likely = hiveCtx.sql("SELECT count(state) as leastlikely,state from ufo_data_part where country = 'us'  group by state order by leastlikely ASC limit 5; ")
     least_likely.show()
  //WHAT state AM I LEAST LIKELY TO SPOT A UFO  
 }
@@ -241,7 +261,7 @@ if(a==8){
   b =false;
 }
 
-}while(b)
+}while(b) 
  
  //val goback = new adminOrUser().choice(scan)
  
@@ -269,7 +289,7 @@ if(a==8){
         //hiveCtx.sql("CREATE TABLE IF NOT EXISTS data1 (datetime String, city STRING, state STRING, country string, shape STRING, duration_seconds String, duration_HoursMin String, comments String, date_posted STRING,latitude STRING, Lngitude STRING) row format delimited fields terminated by ',' stored as textfile; ") 
         //hiveCtx.sql("INSERT INTO data1 SELECT * FROM temp_data")
           
-      val summary = hiveCtx.sql("SELECT * FROM ufo_808_part LIMIT 10;") //this is my partitioned table
+      val summary = hiveCtx.sql("SELECT * FROM ufo_data_part LIMIT 10;") //this is my partitioned table
       summary.show()
       sc.stop()
      
